3924373e5b53a1873eddd17301ea0a5f277dcf17
Marcelo <marcelopbuligon@gmail.com>
2021-09-11 21:57:44 -0300

    API call

---
+++ Marvel-Characters/Marvel-Characters/Models/DataStruct.swift
@@ -0,0 +1,11 @@
+//
+//  DataStruct.swift
+//  Marvel-Characters
+//
+//  Created by Marcelo Pagliarini Buligon on 11/09/21.
+//
+
+struct DataStruct: Decodable {
+    var limit: Int?
+    var results: [Character]?
+}
