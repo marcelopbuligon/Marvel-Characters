1148779e1381418374fe8eaf35eb0850e019d4ff
Marcelo <marcelopbuligon@gmail.com>
2021-09-11 21:09:40 -0300

    Remove storyboard

---
+++ Marvel-Characters/Marvel-Characters/Resources/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  Marvel-Characters
+//
+//  Created by Marcelo Pagliarini Buligon on 11/09/21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let navigation = UINavigationController()
+        
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = navigation
+        window?.makeKeyAndVisible()
+    }
+}
